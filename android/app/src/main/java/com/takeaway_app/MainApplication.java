f64d1489dd364807a66316f08014599ebe56618b
tonydmorris <anthonymorris86@gmail.com>
2020-05-11 17:43:26 +0100

    java edits

--- android/app/src/main/java/com/takeaway_app/MainApplication.java
+++ android/app/src/main/java/com/takeaway_app/MainApplication.java
@@ -9,14 +9,14 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new NavigationReactNativeHost(this) {
+  new NavigationReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
