b0cb8497d95c2bc44619d35e085fd740e1f2fdd2
tonydmorris <anthonymorris86@gmail.com>
2020-05-24 14:22:42 +0100

    working on menu screen

--- App.js
+++ App.js
@@ -12,23 +12,47 @@ import {
   ScrollView,
   StatusBar,
   Image,
-  StyleSheet
+  StyleSheet,
+  ImageBackground,
+  Button,
 } from 'react-native';
 
+import {Navigation} from 'react-native-navigation';
 
-
-const App: () => React$Node = () => {
+const App = props => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-       >
-         <Image style={styles.logo} source={require('./assests/images/thalilogo.png')} />
-            
-          
-        </ScrollView>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require('./assests/images/Mumbai_Dabbawala_or_Tiffin_Wallahs-_200,000_Tiffin_Boxes_Delivered_Per_Day.jpg')}>
+          <ScrollView
+            style={styles.container}
+            contentInsetAdjustmentBehavior="automatic">
+            <Image
+              style={styles.logo}
+              source={require('./assests/images/thalilogo.png')}
+            />
+            <Button
+              title="Menu"
+              onPress={() =>
+                Navigation.push(props.componentId, {
+                  component: {
+                    name: 'Menu',
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Menu',
+                        },
+                      },
+                    },
+                  },
+                })
+              }
+            />
+          </ScrollView>
+        </ImageBackground>
       </SafeAreaView>
     </>
   );
@@ -37,20 +61,31 @@ App.options = {
   topBar: {
     title: {
       text: 'Thali',
-      color: 'black'
+      color: 'black',
     },
     background: {
-      color: '#feecd9'
-    }
-  }
-}
+      color: '#feecd9',
+    },
+  },
+};
 
 const styles = StyleSheet.create({
-  
   logo: {
-    width: 66,
-    height: 58,
-  }
+    width: 200,
+    height: 200,
+    backgroundColor: '#f4f4f4',
+    borderRadius: 100,
+    alignSelf: 'center',
+  },
+  container: {
+    padding: 20,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    overflow: 'hidden',
+    height: 700,
+  },
 });
 
 export default App;
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-import { Navigation } from "react-native-navigation";
-import App from "./App";
+import {Navigation} from 'react-native-navigation';
+import App from './App';
+import Menu from './components/menu/Menu';
 
-Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.registerComponent('com.myApp.Home', () => App);
+Navigation.registerComponent('com.myApp.Menu', () => Menu);
 Navigation.events().registerAppLaunchedListener(() => {
-   Navigation.setRoot({
-     root: {
-       stack: {
-         children: [
-           {
-             component: {
-               name: 'com.myApp.WelcomeScreen'
-             }
-           }  
-         ]
-       }
-     }
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.Home',
+            },
+          },
+        ],
+      },
+    },
   });
 });
