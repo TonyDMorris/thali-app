2ceef6c43b7b5ada5e0bd1337b7b59ad1adeaf62
tonydmorris <anthonymorris86@gmail.com>
2020-06-06 23:30:11 +0100

    wrapped selection modal and menu with basket provider

--- index.js
+++ index.js
@@ -2,24 +2,40 @@ import React from 'react';
 import {Navigation} from 'react-native-navigation';
 import App from './App';
 import Menu from './components/menu/Menu';
-import BasketTopBar from './components/basket/BasketTopBar';
 import {BasketProvider} from './components/basket/BasketContext';
 import SelectionModal from './components/selection/SelectionModal';
 
 Navigation.registerComponent('com.myApp.Home', () => App);
-Navigation.registerComponent('com.myApp.Menu', () => Menu);
-Navigation.registerComponent('com.myApp.SelectionModal', () => SelectionModal);
-Navigation.registerComponent('com.myApp.BasketTopBar', () => BasketTopBar);
-Navigation.setDefaultOptions({
-  statusBar: {
-    backgroundColor: '#4d089a',
+Navigation.registerComponent(
+  'com.myApp.Menu',
+  () => props => {
+    return (
+      <BasketProvider>
+        <Menu {...props} />
+      </BasketProvider>
+    );
   },
+  () => Menu,
+);
+Navigation.registerComponent(
+  'com.myApp.SelectionModal',
+  () => props => {
+    return (
+      <BasketProvider>
+        <SelectionModal {...props} />
+      </BasketProvider>
+    );
+  },
+  () => SelectionModal,
+);
+Navigation.setDefaultOptions({
+  statusBar: {},
   topBar: {
     backButton: {
       color: 'black',
     },
     background: {
-      color: '#f8bac7',
+      color: '#039894',
     },
   },
 });
