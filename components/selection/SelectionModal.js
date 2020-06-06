722ec1c9ccd77b22180e6a09dd3f05f26221fae4
tonydmorris <anthonymorris86@gmail.com>
2020-06-06 23:33:36 +0100

    added basic selection modal to contiue work on options selection

--- components/selection/SelectionModal.js
+++ components/selection/SelectionModal.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {BasketContext} from '../basket/BasketContext';
+import {Navigation} from 'react-native-navigation';
 
-const SelectionModal = props => {
+const SelectionModal = ({item, addItem}) => {
+  console.log(item);
+  const handlePress = () => {
+    const [options, setOptions] = useState([]);
+
+    addItem(item);
+    Navigation.dismissAllModals({
+      animations: {
+        dismissModal: {
+          enable: false,
+        },
+      },
+    });
+  };
   return (
-    <View>
-      <Text>{JSON.stringify(props)}</Text>
+    <View style={styles.wrapper}>
+      {item.food_item_options.map(option => {
+        return (
+          <View>
+            <Text>{option.title}</Text>
+            <Text>{option.description}</Text>
+            <Text>
+              {option.additional_charge > 0 &&
+                `£ ${parseFloat(option.additional_charge).toFixed(2)} extra`}
+            </Text>
+          </View>
+        );
+      })}
+      <Button title="ok" />
     </View>
   );
 };
 
 export default SelectionModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {backgroundColor: 'white', width: '100%', height: '100%'},
+});
