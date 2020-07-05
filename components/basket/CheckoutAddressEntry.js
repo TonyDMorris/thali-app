8116e9609e70219e9e92298c39b27f6e8de8353a
tonydmorris <anthonymorris86@gmail.com>
2020-07-05 15:33:07 +0100

    adding address selection functionality

--- components/basket/CheckoutAddressEntry.js
+++ components/basket/CheckoutAddressEntry.js
@@ -8,15 +8,82 @@ import {
   Image,
   Dimensions,
   ImageBackground,
+  TouchableHighlight,
 } from 'react-native';
-
+import Collapsible from 'react-native-collapsible';
+import stripe from 'tipsi-stripe';
 const CheckoutAddressEntry = ({order}) => {
+  stripe.setOptions({
+    publishableKey:
+      'pk_test_51H0Rn9A5cyq20WwH1ZifHxKJdSCRni4BX1NwnDWzgnCW7hnK81Z2Ie2LlLdb7Cp9foY4Y3rjQfdj30VZMgCfvK2w00synDt5JP',
+
+    androidPayMode: 'test', // Android only
+  });
+
+  const handlePay = async () => {
+    try {
+      const token = await stripe.paymentRequestWithCardForm({
+        total_price: '100.00',
+        currency_code: 'USD',
+        shipping_address_required: true,
+        requiredBillingAddressFields: 'full',
+        prefilledInformation: {
+          billingAddress: {
+            name: 'Gunilla Haugeh',
+            line1: 'Canary Place',
+            line2: '3',
+            city: 'Macon',
+            state: 'Georgia',
+            country: 'US',
+            postalCode: '31217',
+            email: 'ghaugeh0@printfriendly.com',
+          },
+        },
+        phone_number_required: true,
+        shipping_countries: ['US', 'CA'],
+        // line_items: [
+        //   {
+        //     currency_code: 'USD',
+        //     description: 'Whisky',
+        //     total_price: '50.00',
+        //     unit_price: '50.00',
+        //     quantity: '1',
+        //   },
+        //   {
+        //     currency_code: 'USD',
+        //     description: 'Vine',
+        //     total_price: '30.00',
+        //     unit_price: '30.00',
+        //     quantity: '1',
+        //   },
+        //   {
+        //     currency_code: 'USD',
+        //     description: 'Tipsi',
+        //     total_price: '20.00',
+        //     unit_price: '20.00',
+        //     quantity: '1',
+        //   },
+        // ],
+      });
+      console.log(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const [houseNumber, setHouseNumber] = useState('');
   const [address, setAddress] = useState('');
   const [postCode, setPostCode] = useState('');
   const [phone, setPhone] = useState('');
   const [deliveryInstructions, setDeliveryInstructions] = useState('');
 
+  const [
+    manualAddressFormIsCollapsed,
+    setManualAddressFormIsCollapsed,
+  ] = useState(true);
+
+  const [addressDropDownIsCollapsed, setAddressDropDownIsCollapsed] = useState(
+    true,
+  );
   return (
     <ImageBackground
       style={styles.container}
@@ -28,41 +95,53 @@ const CheckoutAddressEntry = ({order}) => {
             style={styles.topLogoImage}
           />
         </View>
-
         <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setHouseNumber(text)}
-            value={houseNumber}
-            placeholder={'house/flat number'}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setAddress(text)}
-            value={address}
-            placeholder={'street address'}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setPostCode(text)}
-            value={postCode}
-            placeholder={'postcode'}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setPhone(text)}
-            value={phone}
-            placeholder={'phone number'}
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setDeliveryInstructions(text)}
-            value={deliveryInstructions}
-            placeholder={'additional instructions'}
-          />
+          <TouchableHighlight>
+            <View>
+              <Text>select postcode</Text>
+            </View>
+          </TouchableHighlight>
+          <Collapsible collapsed={manualAddressFormIsOpen} />
+          <Collapsible collapsed={manualAddressFormIsOpen}>
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setHouseNumber(text)}
+              value={houseNumber}
+              placeholder={'house/flat number'}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setAddress(text)}
+              value={address}
+              placeholder={'street address'}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setPostCode(text)}
+              value={postCode}
+              placeholder={'postcode'}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setPhone(text)}
+              value={phone}
+              placeholder={'phone number'}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => setDeliveryInstructions(text)}
+              value={deliveryInstructions}
+              placeholder={'additional instructions'}
+            />
+          </Collapsible>
         </View>
         <View style={styles.button}>
-          <Button color="#0398aa" raised={true} title="Pay" />
+          <Button
+            onPress={handlePay}
+            color="#0398aa"
+            raised={true}
+            title="Pay"
+          />
         </View>
       </View>
     </ImageBackground>
